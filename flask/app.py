fcc45e187ca42e73ae9a1e25f48dbf5f85bdb288
unknown <taylorvought@gmail.com>
2020-11-04 18:51:38 -0800

    starting flask app

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,24 @@
-# flask
+# flask imports
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+# connection string for Taylor's DB
+conn_string='tester:taylor@localhost:5432/Opiate_Analysis'
+engine= create_engine(f'postgresql://{conn_string}')
+
+app = Flask(__name__)
+
+res=engine.connect().execute('select * from "Prescriber_info" limit 100')
+for row in res:
+    print(row[0])
+
+@app.route("/")
+def home():
+    res=engine.connect().execute('select * from "Prescriber_info"')
+    mydict={}
+    for row in res:
+        mydict[str(row[0])]=row[3]
+    return jsonify(mydict)
